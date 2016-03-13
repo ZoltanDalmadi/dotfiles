365546d33e44dcb416e5f64f6fff570a2d8f0cb0
Zoltan Dalmadi <dmz985@gmail.com>
2016-03-13 17:41:22 +0100

    Changed script to work from any directory

--- configure_urxvt.sh
+++ configure_urxvt.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
+script_dir=$(dirname $(readlink -f $0))
 
 echo -e "Select your color theme:"
 index=0
 
-for theme in urxvt_colors/*; do
+for theme in $script_dir/urxvt_colors/*; do
     themes[$index]=${theme##*/}
     ((index++))
 done
 
 select theme in ${themes[@]}; do
-    ln -sf urxvt_colors/$theme urxvt_theme
+    ln -sf $script_dir/urxvt_colors/$theme $script_dir/urxvt_theme
     break;
 done
 
 echo -e "Select your font:"
 index=0
 
-for font in urxvt_fonts/*; do
+for font in $script_dir/urxvt_fonts/*; do
     fonts[$index]=${font##*/}
     ((index++))
 done
 
 select font in ${fonts[@]}; do
-    ln -sf urxvt_fonts/$font urxvt_font
+    ln -sf $script_dir/urxvt_fonts/$font $script_dir/urxvt_font
     break;
 done
 
-xrdb -I$PWD Xresources
+script_dir=$(dirname $(readlink -f $0))
+xrdb -I$script_dir $script_dir/Xresources
