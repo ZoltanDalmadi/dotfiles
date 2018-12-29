86f193078b6f10155d14ad77dfc9c59e17851e86
Zoltan Dalmadi <dmz985@gmail.com>
2018-12-29 12:35:35 +0100

    [config] Use rofi or dmenu if they are installed

--- configure.sh
+++ configure.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
+
+has_command() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+join() {
+    local IFS="$1"; shift; echo "$*";
+}
+
 script_dir=$(dirname $(readlink -f $0))
 
-echo "Select your color theme:"
 index=0
-
 for theme in $script_dir/colors/*; do
     themes[$index]=${theme##*/}
     ((index++))
 done
 
-select theme in ${themes[@]}; do
-    ln -sf $script_dir/colors/$theme $script_dir/theme
-    break;
-done
-
-echo "Select your font:"
 index=0
-
 for font in $script_dir/fonts/*; do
     fonts[$index]=${font##*/}
     ((index++))
 done
 
-select font in ${fonts[@]}; do
-    ln -sf $script_dir/fonts/$font $script_dir/font
-    break;
-done
+if has_command rofi; then
+    menu="rofi -dmenu"
+elif has_command dmenu; then
+    menu="dmenu"
+fi
+
+if [[ -n $menu ]]; then
+    theme=$(join $'\n' ${themes[@]} | $menu -p "Select your color theme")
+    font=$(join $'\n' ${fonts[@]} | $menu -p "Select your font")
+else
+    echo "Select your color theme:"
+    select theme in ${themes[@]}; do
+        break;
+    done
+
+    echo "Select your font:"
+    select font in ${fonts[@]}; do
+        break;
+    done
+fi
+
+[[ -n $theme ]] && ln -sf $script_dir/colors/$theme $script_dir/theme
+[[ -n $font ]] && ln -sf $script_dir/fonts/$font $script_dir/font
 
 xrdb -I$script_dir $script_dir/Xresources && killall -USR1 st && i3-msg restart
